5959e80dc16a63c49d6aca21cc6d30d0e32b8536
RachanaMaskey <rachanamaskey@gmail.com>
2025-05-26 10:53:18 +0545

    updated media query for navbar

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
-  $('.portfolio-carousel').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    infinite: true
+  $('.slider').slick({
+    slidesToShow: 1,             // How many slides visible
+    slidesToScroll: 1,           // Scroll 1 at a time
+    infinite: true,
+    dots: true,                  // Optional: navigation dots
+    arrows: true,                // Optional: left/right arrows
+    autoplay: true,              // Optional: autoplay
+    autoplaySpeed: 3000,         // Autoplay interval in ms
+    responsive: [
+      // {
+      //   breakpoint: 768,         // Mobile breakpoint
+      //   settings: {
+      //     slidesToShow: 1
+      //   }
+      // },
+      // {
+      //   breakpoint: 1024,
+      //   settings: {
+      //     slidesToShow: 2
+      //   }
+      // }
+    ]
   });
 });
-
-console.log('JS is loaded');
