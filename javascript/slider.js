f8808c35937493bca06c3d1944d171f3d0c7f049
RachanaMaskey <rachanamaskey@gmail.com>
2025-05-26 18:29:22 +0545

    complete responsive

--- javascript/slider.js
+++ javascript/slider.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
          }
        },
        {
-         breakpoint: 350,
+         breakpoint: 430,
          settings: {
            slidesToShow: 1,
            slidesToScroll: 1
