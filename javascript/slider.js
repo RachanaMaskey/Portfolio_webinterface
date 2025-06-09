8d8b0bcbf7f9b9a2f82d49094427eb2297b0e2b8
RachanaMaskey <rachanamaskey@gmail.com>
2025-06-09 19:59:50 +0545

    final changes

--- javascript/slider.js
+++ javascript/slider.js
@@ -21,9 +21,6 @@ $(document).ready(function(){
            slidesToScroll: 1
          }
        }
-       // You can unslick at a given breakpoint now by adding:
-       // settings: "unslick"
-       // instead of a settings object
      ]
    });
  });
